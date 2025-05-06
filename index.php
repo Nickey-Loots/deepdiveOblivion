c8afaca3d69c7b5d4a0012b5a3f0e91b7817a0bc
Nickey Loots <nloots@st.noorderpoort.nl>
2025-05-06 15:50:04 +0200

    git push fetch updated

--- index.php
+++ index.php
@@ -46,24 +46,34 @@ $apiKey = '5b3ce3597851110001cf6248f327d64ee5e8476b8d77831febc00fd0';
 
     const orsCoords = coordinates.map(c => [c[1], c[0]]);
 
-    fetch('https://api.openrouteservice.org/v2/directions/foot-walking/geojson', {
-        method: 'POST',
-        headers: {
-            'Authorization': orsApiKey,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            coordinates: orsCoords
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        const geojson = L.geoJSON(data, {
-            style: { color: 'blue', weight: 4 }
-        }).addTo(map);
-        map.fitBounds(geojson.getBounds());
-    })
-    .catch(err => console.error('ORS Error:', err));
+    async function fetchRoute() {
+        try {
+            const response = await fetch('https://api.openrouteservice.org/v2/directions/foot-walking/geojson', {
+                method: 'POST',
+                headers: {
+                    'Authorization': orsApiKey,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    coordinates: orsCoords
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            const geojson = L.geoJSON(data, {
+                style: { color: 'blue', weight: 4 }
+            }).addTo(map);
+            map.fitBounds(geojson.getBounds());
+        } catch (err) {
+            console.error('ORS Error:', err);
+        }
+    }
+
+    fetchRoute();
 </script>
 
 </body>
