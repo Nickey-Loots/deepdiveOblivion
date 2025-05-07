c9ac5edf442f16a807274b648c667f1a7a8245ee
Myron-Meulenkamp <mdmeulenkamp@st.noorderpoort.nl>
2025-05-07 10:00:19 +0200

    x

--- opdrachten/opdr.js
+++ opdrachten/opdr.js
@@ -1,1 +1,17 @@
-document.getElementById("opdracht").addEventListener("click");
+navigator.geolocation.getCurrentPosition(success, error);
+
+function success(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    let opdracht = 'Geen opdracht gevonden op deze locatie';
+    if (lat > 53 && lon < 6.6) {
+        opdracht = 'Maak foto van duurste kussen';
+    }
+
+    document.getElementById('opdracht').innerHTML = opdracht;
+}
+
+function error() {
+    document.getElementById('opdracht').innerHTML = 'Locatie kon niet worden opgehaald';
+}
